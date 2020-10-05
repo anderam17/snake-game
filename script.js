71177129c173c744457ad71f5166236872955dd5
Allana Anderson <allana.anderson17@gmail.com>
2020-10-05 12:57:51 -0600

    added comments to JS. Created more than one snake segment and added outline to each segment.

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 function draw() {
-    var canvas = $('#canvas')[0];
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
+  var canvas = $("#canvas")[0];
+  var ctx = canvas.getContext("2d");
 
-      ctx.fillStyle = 'rgb(200, 0, 0)';
-      ctx.fillRect(50, 100, 10, 10);
-    }
+  //just an array holding the x and y coordinates for all segments in our snake
+  var snake = [
+    { x: 50, y: 100 },
+    { x: 50, y: 90 },
+    { x: 50, y: 80 },
+  ];
+
+  //setting snakeWidth and snakeHeight both to 10
+  var snakeWidth = (snakeHeight = 10);
+
+  //call drawSnake function
+  drawSnake();
+
+  //draws each section of the snake using the values in the snake array
+  function drawSnake() {
+    $.each(snake, function (index, value) {
+        //color
+      ctx.fillStyle = "rgb(200, 0, 0)";
+      //getting unique x and y coordinates as well as width and height of each block
+      ctx.fillRect(value.x, value.y, snakeWidth, snakeHeight);
+    //   making outline color and location/sizes
+      ctx.strokeStyle = "white";
+      ctx.strokeRect(value.x, value.y, snakeWidth, snakeHeight);
+    });
   }
+}
